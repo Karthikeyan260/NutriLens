4122cfd212138849fbf9bb06cf1619c7b2cd9bbb
Karthikeyan K <140727545+Karthikeyan260@users.noreply.github.com>
2026-03-19 13:58:04 +0530

    Upgrade Gemini model and enhance image input handling
    Updated Gemini model version and improved image handling.

--- ap.py
+++ ap.py
@@ -12,7 +12,7 @@ genai.configure(api_key=os.getenv("GOOGLE_API_KEY"))
 
 # Function to get the response from the Gemini model
 def get_gemini_response(input, image, prompt):
-    model = genai.GenerativeModel('gemini-1.5-flash')
+    model = genai.GenerativeModel('gemini-2.5-flash-lite')
     response = model.generate_content([input, image[0], prompt])
     return response.text
 
